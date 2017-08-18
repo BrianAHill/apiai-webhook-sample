f55fdeba41df32600b1254f2eb4e782dd54074bb
BrianAHill <31046427+BrianAHill@users.noreply.github.com>
2017-08-18 15:56:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -29,12 +29,40 @@ restService.post('/hook', function (req, res) {
             
             console.log('AuthURL: ',AuthURL);
             
-            var request = require('request');
-            request(AuthURL, function (error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    console.log(body) 
-                 }
-            })
+            var https = require('https');
+
+            /**
+             * HOW TO Make an HTTP Call - GET
+             */
+            // options for GET
+            var optionsget = {
+                host : 'www.pcrecruiter.net', // here only the domain name
+                // (no http/https !)
+                port : 443,
+                path : '/rest/api/access-token?DatabaseId=' + DatabaseId + '&Username=' + UserId + '&Password=' + Password + '&AppId=' + AppId + '&ApiKey=' + ApplicationKey, // the rest of the url with parameters if needed
+                method : 'GET' // do GET
+            };
+
+            console.info('Options prepared:');
+            console.info(optionsget);
+            console.info('Do the GET call');
+
+            // do the GET request
+            var reqGet = https.request(optionsget, function(res) {
+                console.log("statusCode: ", res.statusCode);
+                // uncomment it for header details
+            //  console.log("headers: ", res.headers);
+
+
+                res.on('data', function(d) {
+                    console.info('GET result:\n');
+                    process.stdout.write(d);
+                    console.info('\n\nCall completed');
+                });
+
+            });
+
+            reqGet.end();
             
             if (requestBody.result) {
                 speech = '';
