e0ba82538c216a0528383b668bc3c58693906c13
BrianAHill <31046427+BrianAHill@users.noreply.github.com>
2017-08-21 11:06:28 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,56 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-
+var host="www.pcrecruiter.net"
+var port=443
 
 const restService = express();
 restService.use(bodyParser.json());
+
+
+//Gets the session tokena and returns it as a string to use for other calls
+function GetSessionToken(req)
+{
+        var UserId=req.headers.userid;
+        var DatabaseId=req.headers.databaseid;
+        var Password=req.headers.password;
+        var AppId=req.headers.appid;
+        var ApplicationKey=req.headers.applicationkey;
+    
+        var https = require('https');
+
+        // options for GET
+        var optionsget = {
+            host : host, // here only the domain name
+            port : port,
+            path : '/rest/api/access-token?DatabaseId=' + DatabaseId + '&Username=' + UserId + '&Password=' + Password + '&AppId=' + AppId + '&ApiKey=' + ApplicationKey, // the rest of the url with parameters if needed
+            method : 'GET' // do GET
+        };
+
+        console.info('Options prepared:');
+        console.info(optionsget);
+        console.info('Do the GET call');
+
+        // do the GET request
+        var reqGet = https.request(optionsget, function(res) {
+            console.log("statusCode: ", res.statusCode);
+            // uncomment it for header details
+        //  console.log("headers: ", res.headers);
+
+
+            res.on('data', function(d) {
+                console.info('GET result:\n');
+                process.stdout.write(d);
+                console.info('\n\nCall completed');
+            });
+
+        });
+
+        reqGet.end();    
+    
+    
+    
+}
 
 restService.post('/hook', function (req, res) {
 
@@ -13,57 +59,13 @@ restService.post('/hook', function (req, res) {
 //test
     try {
 
-        console.log(req.headers);
         var speech = 'empty speech';
-        var UserId=req.headers.userid;
-        var DatabaseId=req.headers.databaseid;
-        var Password=req.headers.password;
-        var AppId=req.headers.appid;
-        var ApplicationKey=req.headers.applicationkey;
-     
+   
       
          if (req.body) {
             var requestBody = req.body;
-
-            var AuthURL='https://www.pcrecruiter.net/rest/api/access-token?DatabaseId=' + DatabaseId + '&Username=' + UserId + '&Password=' + Password + '&AppId=' + AppId + '&ApiKey=' + ApplicationKey;
-            
-            console.log('AuthURL: ',AuthURL);
-            
-            var https = require('https');
-
-            /**
-             * HOW TO Make an HTTP Call - GET
-             */
-            // options for GET
-            var optionsget = {
-                host : 'www.pcrecruiter.net', // here only the domain name
-                // (no http/https !)
-                port : 443,
-                path : '/rest/api/access-token?DatabaseId=' + DatabaseId + '&Username=' + UserId + '&Password=' + Password + '&AppId=' + AppId + '&ApiKey=' + ApplicationKey, // the rest of the url with parameters if needed
-                method : 'GET' // do GET
-            };
-
-            console.info('Options prepared:');
-            console.info(optionsget);
-            console.info('Do the GET call');
-
-            // do the GET request
-            var reqGet = https.request(optionsget, function(res) {
-                console.log("statusCode: ", res.statusCode);
-                // uncomment it for header details
-            //  console.log("headers: ", res.headers);
-
-
-                res.on('data', function(d) {
-                    console.info('GET result:\n');
-                    process.stdout.write(d);
-                    console.info('\n\nCall completed');
-                });
-
-            });
-
-            reqGet.end();
-            
+       
+    
             if (requestBody.result) {
                 speech = '';
 
