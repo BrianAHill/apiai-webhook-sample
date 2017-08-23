10e31dd977846bf579d16f12ebcdf4bb642858ec
BrianAHill <31046427+BrianAHill@users.noreply.github.com>
2017-08-23 15:07:10 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,113 +2,42 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-var host="www.pcrecruiter.net"
-var port=443
+
 
 const restService = express();
 restService.use(bodyParser.json());
-var https = require('https');
-
-function GetJSON(options,cb)
-{
-        https.request(options,function(res){
-                var body='';
-                
-                res.on('data',function(chunk){
-                        body+=chunk;
-                });
-                              
-                res.on('end',function(){
-                        var result=JSON.parse(body);
-                        cb(null,result);
-                });    
-                
-                res.on('error',cb);
-                
-                }).end();                
-}
-
-//Gets the session tokena and returns it as a string to use for other calls
-function GetSessionToken(req,cb)
-{
-        var UserId=req.headers.userid;
-        var DatabaseId=req.headers.databaseid;
-        var Password=req.headers.password;
-        var AppId=req.headers.appid;
-        var ApplicationKey=req.headers.applicationkey;
-        var TokenReturn;
-
-
-        // options for GET
-        var optionsget = {
-            host : host, // here only the domain name
-            port : port,
-            path : '/rest/api/access-token?DatabaseId=' + DatabaseId + '&Username=' + UserId + '&Password=' + Password + '&AppId=' + AppId + '&ApiKey=' + ApplicationKey, // the rest of the url with parameters if needed
-            method : 'GET' // do GET
-        };
-
-        GetJSON(optionsget,function(err,result){
-                if(err){
-                        return console.log('Error getting Token: ',err);       
-                }
-                cb(null,result);
-        });
-        
-       
-   
-}
 
 restService.post('/hook', function (req, res) {
 
-    var Token;
-    var FirstName;
-    var LastName;
-        
     console.log('hook request');
+
     try {
-
         var speech = 'empty speech';
-   
-        GetSessionToken(req,function(err,result){
 
-                if(err){
-                        return console.log('Error getting Token: ',err);       
-                }
-        
-                Token=result.SessionId;
-                              
-                
-                 if (req.body) {
-                    var requestBody = req.body;
-                    FirstName=req.body.parameters.First-Name;
-                    LastName=req.body.parameters.Last-Name;
-                      
-                    if (requestBody.result) {
-                        speech = '';
+        if (req.body) {
+            var requestBody = req.body;
 
-                        if (requestBody.result.fulfillment) {
-                            speech += requestBody.result.fulfillment.speech;
-                            speech += 'You would like to add a note to ' + FirstName + ' ' + LastName + '?';
-                        }
+            if (requestBody.result) {
+                speech = '';
 
-                        if (requestBody.result.action) {
-                            speech += 'action: ' + requestBody.result.action;
-                        }
-                    }
+                if (requestBody.result.fulfillment) {
+                    speech += requestBody.result.fulfillment.speech;
+                    speech += ' ';
                 }
 
-                console.log('result: ', speech);
+                if (requestBody.result.action) {
+                    speech += 'action: ' + requestBody.result.action;
+                }
+            }
+        }
 
-                return res.json({
-                    speech: speech,
-                    displayText: speech,
-                    source: 'apiai-webhook-sample'
-                });
-                
-                
-        });          
-            
-          
+        console.log('result: ', speech);
+
+        return res.json({
+            speech: speech,
+            displayText: speech,
+            source: 'apiai-webhook-sample'
+        });
     } catch (err) {
         console.error("Can't process request", err);
 
