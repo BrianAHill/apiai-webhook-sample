b7566f522be4e32dde0486cf9b7908d118ae5381
BrianAHill <31046427+BrianAHill@users.noreply.github.com>
2017-08-23 15:02:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,64 +1,126 @@
 'use strict';
-const http = require('https');
-const host = 'www.pcrecruiter.net';
 
+const express = require('express');
+const bodyParser = require('body-parser');
+var host="www.pcrecruiter.net"
+var port=443
+
+const restService = express();
+restService.use(bodyParser.json());
+var https = require('https');
+
+function GetJSON(options,cb)
+{
+        https.request(options,function(res){
+                var body='';
+                
+                res.on('data',function(chunk){
+                        body+=chunk;
+                });
+                              
+                res.on('end',function(){
+                        var result=JSON.parse(body);
+                        cb(null,result);
+                });    
+                
+                res.on('error',cb);
+                
+                }).end();                
+}
+
+//Gets the session tokena and returns it as a string to use for other calls
+function GetSessionToken(req,cb)
+{
+        var UserId=req.headers.userid;
+        var DatabaseId=req.headers.databaseid;
+        var Password=req.headers.password;
+        var AppId=req.headers.appid;
+        var ApplicationKey=req.headers.applicationkey;
+        var TokenReturn;
+
+
+        // options for GET
+        var optionsget = {
+            host : host, // here only the domain name
+            port : port,
+            path : '/rest/api/access-token?DatabaseId=' + DatabaseId + '&Username=' + UserId + '&Password=' + Password + '&AppId=' + AppId + '&ApiKey=' + ApplicationKey, // the rest of the url with parameters if needed
+            method : 'GET' // do GET
+        };
+
+        GetJSON(optionsget,function(err,result){
+                if(err){
+                        return console.log('Error getting Token: ',err);       
+                }
+                cb(null,result);
+        });
+        
+       
+   
+}
 
 restService.post('/hook', function (req, res) {
 
-  console.log('here');
-
-    // Get the city and date from the request
-  var FirstName = req.body.result.parameters['FirstName']; // first name required
-  var LastName = req.body.result.parameters['LastName']; // last name required
-  var UserId=req.headers.userid;
-  var DatabaseId=req.headers.databaseid;
-  var Password=req.headers.password;
-  var AppId=req.headers.appid;
-  var ApplicationKey=req.headers.applicationkey;
-
-  var AuthURL=encodeURI(host + '/rest/api/access-token?DatabaseId=' + DatabaseId + '&Username=' + UserId + '&Password=' + Password + '&AppId=' + AppId + '&ApiKey=' + ApplicationKey);      
+    var Token;
+    var FirstName;
+    var LastName;
         
-  getRequest(AuthURL).then(function (body1) {
-     var authResponse = JSON.parse(body1);
-     var SearchCandidateURL=encodeURI(host + '/rest/api/candidates?Query=FirstName eq ' + FirstName + ' and LastName eq ' + LastName + '&ResultsPerPage=25&SessionId=' + authResponse.SessionId);
-     return getRequest(SearchCandidateURL);
-  }).then(function (body2) {
-     //Count the number of candididates that came back
-     var objCandidates=JSON.parse(body2);
-     var ResultCount=objCanidates.TotalRecords;
-     if(ResultCount>1)
-     {
-        output="Too many results returned, please narrow down your results by Company.";
-     }
-     else if(ResultCount==0)
-     {
-        output="We could not find that record.";
-     }
-     else
-     {
-        output="RecordFound";
-     }
+    console.log('hook request');
+    try {
 
-     res.setHeader('Content-Type', 'application/json');
-     res.send(JSON.stringify({ 'speech': output, 'displayText': output }));
+        var speech = 'empty speech';
+   
+        GetSessionToken(req,function(err,result){
+
+                if(err){
+                        return console.log('Error getting Token: ',err);       
+                }
+        
+                Token=result.SessionId;
+                              
+                
+                 if (req.body) {
+                    var requestBody = req.body;
+                    FirstName=req.body.parameters.First-Name;
+                    LastName=req.body.parameters.Last-Name;
+                      
+                    if (requestBody.result) {
+                        speech = '';
+
+                        if (requestBody.result.fulfillment) {
+                            speech += requestBody.result.fulfillment.speech;
+                            speech += 'You would like to add a note to ' + FirstName + ' ' + LastName + '?';
+                        }
+
+                        if (requestBody.result.action) {
+                            speech += 'action: ' + requestBody.result.action;
+                        }
+                    }
+                }
+
+                console.log('result: ', speech);
+
+                return res.json({
+                    speech: speech,
+                    displayText: speech,
+                    source: 'apiai-webhook-sample'
+                });
+                
+                
+        });          
+            
           
-  }).catch((error) => {
-     // If there is an error let the user know
-     res.setHeader('Content-Type', 'application/json');
-     res.send(JSON.stringify({ 'speech': error, 'displayText': error }));
-  });   
+    } catch (err) {
+        console.error("Can't process request", err);
 
-});
-
-function getRequest(url) {
-  var request = require('request');  
-  return new Promise(function (success, failure) {
-        request(url, function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                success(body);
-            } else {
-                failure(error);
+        return res.status(400).json({
+            status: {
+                code: 400,
+                errorType: err.message
             }
         });
-    });
-}
+    }
+});
+
+restService.listen((process.env.PORT || 5000), function () {
+    console.log("Server listening");
+});
