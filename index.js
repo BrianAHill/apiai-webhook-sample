4d39565ef77e0354b8d6e4033b817dc00994a687
BrianAHill <31046427+BrianAHill@users.noreply.github.com>
2017-08-24 11:02:52 -0400

    Update index.js

--- index.js
+++ index.js
@@ -113,10 +113,11 @@ function getRequest(strpath,cb) {
             res.on('end', function() {
                 console.info('GET result:\n');
                 console.log(buffer);
-                cb(buffer);
+                res=buffer;
                 console.info('\n\nCall completed');
             });
 
+           return res;
         });
         reqGet.on('error', function(e) {
             console.error(e);
